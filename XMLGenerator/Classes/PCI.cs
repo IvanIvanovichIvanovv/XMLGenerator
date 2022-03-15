4fcec40b70eae9caf3ffd9d6ffe1d2559b92ce49
dgolembx <dgolembx@DGOLEMBX-MOBL2>
2022-03-15 14:22:41 +0100

    Add project files.

---
+++ XMLGenerator/Classes/PCI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLGenerator.Classes
+{
+    public class PCI
+    {
+        private string bus;
+        private string slot;
+        private int function;
+
+        public PCI(string bus, string slot, int funtion) 
+        {
+            this.bus = bus;
+            this.slot = slot;   
+            this.function = funtion;
+        }
+        public override string ToString()
+        {
+            return $"bus='0x{bus}' slot='0x{slot}' function='0x{function}'";
+        }
+    }
+}
---
+++ XMLGenerator/Classes/VirtualMachine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLGenerator.Classes
+{
+    public class VirtualMachine
+    {
+        private string name;
+        private int memoryRAM;
+        private int coresCPU;
+        private string macAddress;
+        private PCI pci;
+        private int portVNC;
+
+        public VirtualMachine(string name, int memoryRAM, int coresCPU, string macAddress, PCI pci, int portVNC)
+        {
+            this.name = name;
+            this.memoryRAM = memoryRAM;
+            this.coresCPU = coresCPU;
+            this.macAddress = macAddress;
+            this.pci = pci;
+            this.portVNC = portVNC;
+        }
+
+    }
+}
